d5754396ecdcde6ade05a6df7b94b4fb3cc237cb
Simbilimbi <simbarashempofu53@gmail.com>
2024-07-12 10:26:04 +0200

    git push

--- components/featuredStore.vue
+++ components/featuredStore.vue
@@ -1,40 +1,40 @@
 <template>
     <div class="surface-ground px-4 py-8 md:px-6 lg:px-8">
         <header class="flex w-full justify-content-between align-items-center  surface-border pb-5">
-        <div>
-            <h2 class="mt-0 mb-3 font-medium text-2xl text-900">OK Zimbabwe</h2>
-        </div>
-        <Button class="shopnow feat" label="Shop Now" />
+            <div>
+                <h2 class="mt-0 mb-3 font-medium text-2xl text-900">OK Zimbabwe</h2>
+            </div>
+            <Button class="shopnow feat" label="Shop Now" />
         </header>
-        <Carousel :value="products" :numVisible="5" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="8000">
+        <Carousel :value="dummyProducts" :numVisible="5" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
             <template #item="slotProps">
                 <div class="border-1 surface-border border-round m-2 p-3">
                     <div class="surface-50 flex align-items-center justify-content-center mb-3 mx-auto">
-                <img :src="slotProps.data.images[0]" class="w-full h-full object-cover">
-                </div>
-                    <div class="mb-3 font-medium nametext">{{ addEllipsis(slotProps.data.title) }}</div>
+                        <img :src="slotProps.data.image" class="w-full h-full object-cover">
+                    </div>
+                    <div class="mb-3 font-medium nametext">{{ addEllipsis(slotProps.data.name) }}</div>
                     <div class="mb-4">
-                </div>
-                <div class="flex justify-content-between align-items-center">
-                    <span class="font-bold text-900 ml-2">${{ slotProps.data.price }}</span>
-                    <Button @click="navigateTo(`${slotProps.data.redirect_url}`,{external: true})" icon="pi pi-cart-arrow-down" label="Add" class="ml-auto cart"/>
-                </div>
+                    </div>
+                    <div class="flex justify-content-between align-items-center">
+                        <span class="font-bold text-900 ml-2">${{ slotProps.data.price }}</span>
+                        <Button @click="navigateTo(slotProps.data.redirect_url, {external: true})" icon="pi pi-cart-arrow-down" label="Add" class="ml-auto cart"/>
+                    </div>
                 </div>
             </template>
         </Carousel>
-        <Carousel :value="products" :numVisible="5" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="8000">
+        <Carousel :value="products" :numVisible="5" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
             <template #item="slotProps">
                 <div class="border-1 surface-border border-round m-2 p-3">
                     <div class="surface-50 flex align-items-center justify-content-center mb-3 mx-auto">
-                <img :src="slotProps.data.images[0]" class="w-full h-full object-cover">
-                </div>
-                    <div class="mb-3 font-medium nametext">{{ addEllipsis(slotProps.data.title) }}</div>
+                        <img :src="slotProps.data.image" class="w-full h-full object-cover">
+                    </div>
+                    <div class="mb-3 font-medium nametext">{{ addEllipsis(slotProps.data.name) }}</div>
                     <div class="mb-4">
-                </div>
-                <div class="flex justify-content-between align-items-center">
-                    <span class="font-bold text-900 ml-2">${{ slotProps.data.price }}</span>
-                    <Button @click="navigateTo(`${slotProps.data.redirect_url}`,{external: true})" icon="pi pi-cart-arrow-down" label="Add" class="ml-auto cart"/>
-                </div>
+                    </div>
+                    <div class="flex justify-content-between align-items-center">
+                        <span class="font-bold text-900 ml-2">${{ slotProps.data.price }}</span>
+                        <Button @click="navigateTo(slotProps.data.redirect_url, {external: true})" icon="pi pi-cart-arrow-down" label="Add" class="ml-auto cart"/>
+                    </div>
                 </div>
             </template>
         </Carousel>
@@ -43,77 +43,154 @@
 
 <script lang="ts" setup>
 const op = ref();
-const products:any = ref([]);
 
+
+// Dummy products data
 const dummyProducts = [
     {
-        shop_logo: '/images/logos/okzim.jpg',
-        name: 'Product 1',
-        image: '/images/products/product1.jpg',
+        shop_logo: '/images/logos/nivea.jpg',
+        name: 'Nivea Body Lotion Q10 400ml',
+        image: '/images/products/nivea.jpg',
         price: '29.99',
         redirect_url: '/product/1'
     },
     {
         shop_logo: '/images/logos/shop2.jpg',
-        name: 'Product 2',
-        image: '/images/products/product2.jpg',
+        name: "Babysoft 18's",
+        image: '/images/products/tissue.jpg',
         price: '39.99',
         redirect_url: '/product/2'
     },
     {
         shop_logo: '/images/logos/shop3.jpg',
-        name: 'Product 3',
-        image: '/images/products/product3.jpg',
+        name: 'Vaseline Blue Seal Petroleum Jelly 100ml',
+        image: '/images/products/vaseline.jpg',
         price: '49.99',
         redirect_url: '/product/3'
     },
     {
         shop_logo: '/images/logos/shop4.jpg',
-        name: 'Product 4',
-        image: '/images/products/product4.jpg',
+        name: 'Sunlight Dishwasher Lemon 750ml',
+        image: '/images/products/sunlight.jpg',
         price: '59.99',
         redirect_url: '/product/4'
     },
     {
         shop_logo: '/images/logos/shop5.jpg',
-        name: 'Product 5',
-        image: '/images/products/product5.jpg',
+        name: 'Harpic Toilet Cleaner Potpourri 500ml',
+        image: '/images/products/harpic.jpg',
         price: '69.99',
         redirect_url: '/product/5'
     },
     {
         shop_logo: '/images/logos/shop6.jpg',
-        name: 'Product 6',
-        image: '/images/products/product6.jpg',
+        name: 'Handy And All Purpose Cleaner Lavendar 750ml',
+        image: '/images/products/handy_andy.jpg',
         price: '79.99',
         redirect_url: '/product/6'
     },
     {
         shop_logo: '/images/logos/shop7.jpg',
-        name: 'Product 7',
-        image: '/images/products/product7.jpg',
+        name: 'Raid',
+        image: '/images/products/raid.jpg',
         price: '89.99',
         redirect_url: '/product/7'
     },
     {
         shop_logo: '/images/logos/shop8.jpg',
-        name: 'Product 8',
-        image: '/images/products/product8.jpg',
+        name: 'Sta Soft',
+        image: '/images/sta-soft.jpg',
+        price: '99.99',
+        redirect_url: '/product/8'
+    },
+    {
+        shop_logo: '/images/logos/shop8.jpg',
+        name: 'Geisha Soup',
+        image: '/images/products/geisha.jpg',
+        price: '99.99',
+        redirect_url: '/product/8'
+    },
+    {
+        shop_logo: '/images/logos/shop8.jpg',
+        name: 'Dettol Anti Bacterial Soap Even Tone 175g',
+        image: '/images/products/detol.jpg',
         price: '99.99',
         redirect_url: '/product/8'
     }
 ];
-
-onMounted(async () => {
-    try {
-        const response = await fetch('https://dummyjson.com/products');
-        const data = await response.json();
-        products.value = data.products;
-        console.log(products.value) // assuming the response has a 'products' array
-    } catch (error) {
-        console.error('Failed to fetch products:', error);
+const products = [
+    {
+        shop_logo: '/images/logos/nivea.jpg',
+        name: 'Mazoe Orange Crush 2 Ltrs',
+        image: '/images/products/mazoe.jpg',
+        price: '29.99',
+        redirect_url: '/product/1'
+    },
+    {
+        shop_logo: '/images/logos/shop2.jpg',
+        name: "Red Seal Salt 1kg",
+        image: '/images/products/salt.jpg',
+        price: '39.99',
+        redirect_url: '/product/2'
+    },
+    {
+        shop_logo: '/images/logos/shop3.jpg',
+        name: 'Mahatma White Rice 2kg',
+        image: '/images/products/rice.jpg',
+        price: '49.99',
+        redirect_url: '/product/3'
+    },
+    {
+        shop_logo: '/images/logos/shop4.jpg',
+        name: 'Nestle everyday 400g',
+        image: '/images/products/everyday.jpg',
+        price: '59.99',
+        redirect_url: '/product/4'
+    },
+    {
+        shop_logo: '/images/logos/shop5.jpg',
+        name: 'Cashel Valley Baked Beans 410g',
+        image: '/images/products/beans.jpg',
+        price: '69.99',
+        redirect_url: '/product/5'
+    },
+    {
+        shop_logo: '/images/logos/shop6.jpg',
+        name: 'Ekono Spaghetti 400g',
+        image: '/images/products/spaghetti.jpg',
+        price: '79.99',
+        redirect_url: '/product/6'
+    },
+    {
+        shop_logo: '/images/logos/shop7.jpg',
+        name: 'RabRoy Tomato Sauce 375ml',
+        image: '/images/products/sauce.jpg',
+        price: '89.99',
+        redirect_url: '/product/7'
+    },
+    {
+        shop_logo: '/images/logos/shop8.jpg',
+        name: 'Jacobs Coffee Kronung 100g',
+        image: '/images/products/jacobs_coffee.jpg',
+        price: '99.99',
+        redirect_url: '/product/8'
+    },
+    {
+        shop_logo: '/images/logos/shop8.jpg',
+        name: 'Mamas Peanut Butter ',
+        image: '/images/products/peanut.jpg',
+        price: '99.99',
+        redirect_url: '/product/8'
+    },
+    {
+        shop_logo: '/images/logos/shop8.jpg',
+        name: 'Roller Meal',
+        image: '/images/products/roller_meal.jpg',
+        price: '99.99',
+        redirect_url: '/product/8'
     }
-});
+];
+// Set the products to dummyProducts
 
 
 const toggle = (event:any) => {
