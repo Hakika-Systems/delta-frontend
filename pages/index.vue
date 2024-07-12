77beac9ca85ce84bd4f11bf4fada615565fdd376
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-07-12 13:01:22 +0200

    added important text

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
     <div class="border-1 surface-border surface-card border-round p-3 flex flex-column md:flex-row align-items-center z-1">
       <i class="pi pi-android topicons text-2xl md:text-4xl mb-2 md:mb-0 mr-0 md:mr-3"></i>
       <div>
-        <div class="text-600 topcardstext font-medium mb-1">GET THE SHOPPER CHOICE APP</div>
+        <div class="text-900 topcardstext font-medium mb-1">GET THE SHOPPER CHOICE APP</div>
         <span class="text-600 text-sm hidden md:block">Mall in your pocket</span>
       </div>
     </div>
