3ad30d76df277adb9752a57c5a8637bc8b6490f3
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-07-14 08:56:19 +0200

    nuxi error

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -25,7 +25,7 @@ import { consola } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_m
 import { getContext } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/unctx/dist/index.mjs';
 import * as vue$1 from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/vue/index.mjs';
 import { isVNode, version, unref } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/vue/index.mjs';
-import BaseStyle$3 from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/@primevue/core/base/style/index.mjs';
+import BaseStyle$8 from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/@primevue/core/base/style/index.mjs';
 import { Theme } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/@primeuix/styled/index.mjs';
 import { createServerHead as createServerHead$1, CapoPlugin } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/unhead/dist/index.mjs';
 import { defineHeadPlugin } from 'file://C:/Users/user/Documents/GitHub/okzimbabwe/node_modules/@unhead/shared/dist/index.mjs';
@@ -8061,13 +8061,13 @@ function _defineProperty$1(obj, key, value) { key = _toPropertyKey$1(key); if (k
 function _toPropertyKey$1(t) { var i = _toPrimitive$1(t, "string"); return "symbol" == _typeof$1(i) ? i : String(i); }
 function _toPrimitive$1(t, r) { if ("object" != _typeof$1(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof$1(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var css = "\n.p-hidden-accessible {\n    border: 0;\n    clip: rect(0 0 0 0);\n    height: 1px;\n    margin: -1px;\n    overflow: hidden;\n    padding: 0;\n    position: absolute;\n    width: 1px;\n}\n\n.p-hidden-accessible input,\n.p-hidden-accessible select {\n    transform: scale(0);\n}\n\n.p-overflow-hidden {\n    overflow: hidden;\n    padding-right: var(--scrollbar-width);\n}\n";
-var classes$2 = {};
-var inlineStyles = {};
-var BaseStyle$2 = {
+var classes$7 = {};
+var inlineStyles$2 = {};
+var BaseStyle$7 = {
   name: 'base',
   css: css,
-  classes: classes$2,
-  inlineStyles: inlineStyles,
+  classes: classes$7,
+  inlineStyles: inlineStyles$2,
   loadStyle: function loadStyle() {
     var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
     return this.css ? usestyle.useStyle(this.css, _objectSpread({
@@ -8095,19 +8095,19 @@ var BaseStyle$2 = {
   }
 };
 
-var basestyle_cjs = BaseStyle$2;
+var basestyle_cjs = BaseStyle$7;
 
-var BaseStyle$1 = basestyle_cjs;
+var BaseStyle$6 = basestyle_cjs;
 
-function _interopDefaultLegacy$1 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+function _interopDefaultLegacy$6 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var BaseStyle__default$1 = /*#__PURE__*/_interopDefaultLegacy$1(BaseStyle$1);
+var BaseStyle__default$6 = /*#__PURE__*/_interopDefaultLegacy$6(BaseStyle$6);
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-var classes$1 = {
+var classes$6 = {
   root: function root(_ref) {
     var instance = _ref.instance,
       props = _ref.props;
@@ -8132,22 +8132,22 @@ var classes$1 = {
   },
   label: 'p-button-label'
 };
-var ButtonStyle = BaseStyle__default$1["default"].extend({
+var ButtonStyle = BaseStyle__default$6["default"].extend({
   name: 'button',
-  classes: classes$1
+  classes: classes$6
 });
 
 var buttonstyle_cjs = ButtonStyle;
 
 const ButtonStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(buttonstyle_cjs);
 
-var BaseStyle = basestyle_cjs;
+var BaseStyle$5 = basestyle_cjs;
 
-function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+function _interopDefaultLegacy$5 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-var BaseStyle__default = /*#__PURE__*/_interopDefaultLegacy(BaseStyle);
+var BaseStyle__default$5 = /*#__PURE__*/_interopDefaultLegacy$5(BaseStyle$5);
 
-var classes = {
+var classes$5 = {
   root: function root(_ref) {
     var instance = _ref.instance;
     return ['p-carousel p-component', {
@@ -8205,14 +8205,224 @@ var classes = {
   indicatorButton: 'p-link',
   footer: 'p-carousel-footer'
 };
-var CarouselStyle = BaseStyle__default["default"].extend({
+var CarouselStyle = BaseStyle__default$5["default"].extend({
   name: 'carousel',
-  classes: classes
+  classes: classes$5
 });
 
 var carouselstyle_cjs = CarouselStyle;
 
 const CarouselStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(carouselstyle_cjs);
+
+var BaseStyle$4 = basestyle_cjs;
+
+function _interopDefaultLegacy$4 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var BaseStyle__default$4 = /*#__PURE__*/_interopDefaultLegacy$4(BaseStyle$4);
+
+var classes$4 = {
+  root: function root(_ref) {
+    var instance = _ref.instance,
+      props = _ref.props;
+    return ['p-inputtext p-component', {
+      'p-filled': instance.filled,
+      'p-inputtext-sm': props.size === 'small',
+      'p-inputtext-lg': props.size === 'large',
+      'p-invalid': props.invalid,
+      'p-variant-filled': props.variant ? props.variant === 'filled' : instance.$primevue.config.inputStyle === 'filled'
+    }];
+  }
+};
+var InputTextStyle = BaseStyle__default$4["default"].extend({
+  name: 'inputtext',
+  classes: classes$4
+});
+
+var inputtextstyle_cjs = InputTextStyle;
+
+const InputTextStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(inputtextstyle_cjs);
+
+var BaseStyle$3 = basestyle_cjs;
+
+function _interopDefaultLegacy$3 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var BaseStyle__default$3 = /*#__PURE__*/_interopDefaultLegacy$3(BaseStyle$3);
+
+var classes$3 = {
+  root: 'p-inputgroup'
+};
+var InputGroupStyle = BaseStyle__default$3["default"].extend({
+  name: 'inputgroup',
+  classes: classes$3
+});
+
+var inputgroupstyle_cjs = InputGroupStyle;
+
+const InputGroupStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(inputgroupstyle_cjs);
+
+var BaseStyle$2 = basestyle_cjs;
+
+function _interopDefaultLegacy$2 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var BaseStyle__default$2 = /*#__PURE__*/_interopDefaultLegacy$2(BaseStyle$2);
+
+var inlineStyles$1 = {
+  submenu: function submenu(_ref) {
+    var instance = _ref.instance,
+      processedItem = _ref.processedItem;
+    return {
+      display: instance.isItemActive(processedItem) ? 'block' : 'none'
+    };
+  }
+};
+var classes$2 = {
+  root: function root(_ref2) {
+    var instance = _ref2.instance,
+      props = _ref2.props;
+    return ['p-tieredmenu p-component', {
+      'p-tieredmenu-overlay': props.popup,
+      'p-ripple-disabled': instance.$primevue.config.ripple === false
+    }];
+  },
+  start: 'p-tieredmenu-start',
+  menu: 'p-tieredmenu-root-list',
+  menuitem: function menuitem(_ref3) {
+    var instance = _ref3.instance,
+      processedItem = _ref3.processedItem;
+    return ['p-menuitem', {
+      'p-menuitem-active p-highlight': instance.isItemActive(processedItem),
+      'p-focus': instance.isItemFocused(processedItem),
+      'p-disabled': instance.isItemDisabled(processedItem)
+    }];
+  },
+  content: 'p-menuitem-content',
+  action: 'p-menuitem-link',
+  icon: 'p-menuitem-icon',
+  text: 'p-menuitem-text',
+  submenuIcon: 'p-submenu-icon',
+  submenu: 'p-submenu-list',
+  separator: 'p-menuitem-separator',
+  end: 'p-tieredmenu-end'
+};
+var TieredMenuStyle = BaseStyle__default$2["default"].extend({
+  name: 'tieredmenu',
+  classes: classes$2,
+  inlineStyles: inlineStyles$1
+});
+
+var tieredmenustyle_cjs = TieredMenuStyle;
+
+const TieredMenuStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(tieredmenustyle_cjs);
+
+var BaseStyle$1 = basestyle_cjs;
+
+function _interopDefaultLegacy$1 (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var BaseStyle__default$1 = /*#__PURE__*/_interopDefaultLegacy$1(BaseStyle$1);
+
+var inlineStyles = {
+  submenu: function submenu(_ref) {
+    var instance = _ref.instance,
+      processedItem = _ref.processedItem;
+    return {
+      display: instance.isItemActive(processedItem) ? 'block' : 'none'
+    };
+  }
+};
+var classes$1 = {
+  root: function root(_ref2) {
+    var instance = _ref2.instance;
+    return ['p-megamenu p-component', {
+      'p-megamenu-mobile': instance.queryMatches,
+      'p-megamenu-mobile-active': instance.mobileActive,
+      'p-megamenu-horizontal': instance.horizontal,
+      'p-megamenu-vertical': instance.vertical
+    }];
+  },
+  start: 'p-megamenu-start',
+  menubutton: 'p-megamenu-button',
+  menu: 'p-megamenu-root-list',
+  submenuHeader: function submenuHeader(_ref3) {
+    var instance = _ref3.instance,
+      processedItem = _ref3.processedItem;
+    return ['p-megamenu-submenu-header p-submenu-header', {
+      'p-disabled': instance.isItemDisabled(processedItem)
+    }];
+  },
+  menuitem: function menuitem(_ref4) {
+    var instance = _ref4.instance,
+      processedItem = _ref4.processedItem;
+    return ['p-menuitem', {
+      'p-menuitem-active p-highlight': instance.isItemActive(processedItem),
+      'p-focus': instance.isItemFocused(processedItem),
+      'p-disabled': instance.isItemDisabled(processedItem)
+    }];
+  },
+  content: 'p-menuitem-content',
+  action: 'p-menuitem-link',
+  icon: 'p-menuitem-icon',
+  label: 'p-menuitem-text',
+  submenuIcon: 'p-submenu-icon',
+  panel: 'p-megamenu-panel',
+  grid: 'p-megamenu-grid',
+  column: function column(_ref5) {
+    var instance = _ref5.instance,
+      processedItem = _ref5.processedItem;
+    var length = instance.isItemGroup(processedItem) ? processedItem.items.length : 0;
+    var columnClass;
+    if (instance.$parentInstance.queryMatches) columnClass = 'p-megamenu-col-12';else {
+      switch (length) {
+        case 2:
+          columnClass = 'p-megamenu-col-6';
+          break;
+        case 3:
+          columnClass = 'p-megamenu-col-4';
+          break;
+        case 4:
+          columnClass = 'p-megamenu-col-3';
+          break;
+        case 6:
+          columnClass = 'p-megamenu-col-2';
+          break;
+        default:
+          columnClass = 'p-megamenu-col-12';
+          break;
+      }
+    }
+    return columnClass;
+  },
+  submenu: 'p-submenu-list p-megamenu-submenu',
+  submenuLabel: 'p-menuitem-text',
+  separator: 'p-menuitem-separator',
+  end: 'p-megamenu-end'
+};
+var MegaMenuStyle = BaseStyle__default$1["default"].extend({
+  name: 'megamenu',
+  classes: classes$1,
+  inlineStyles: inlineStyles
+});
+
+var megamenustyle_cjs = MegaMenuStyle;
+
+const MegaMenuStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(megamenustyle_cjs);
+
+var BaseStyle = basestyle_cjs;
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var BaseStyle__default = /*#__PURE__*/_interopDefaultLegacy(BaseStyle);
+
+var classes = {
+  root: 'p-badge p-component'
+};
+var BadgeDirectiveStyle = BaseStyle__default["default"].extend({
+  name: 'badge',
+  classes: classes
+});
+
+var badgedirectivestyle_cjs = BadgeDirectiveStyle;
+
+const BadgeDirectiveStyle$1 = /*@__PURE__*/getDefaultExportFromCjs(badgedirectivestyle_cjs);
 
 const runtimeConfig = useRuntimeConfig();
 const config = runtimeConfig?.public?.primevue ?? {};
@@ -8224,14 +8434,14 @@ const styleProps = {
 };
 const styles$2 = [
   ,
-  BaseStyle$3 && BaseStyle$3.getStyleSheet ? BaseStyle$3.getStyleSheet(undefined, styleProps) : '',ButtonStyle$1 && ButtonStyle$1.getStyleSheet ? ButtonStyle$1.getStyleSheet(undefined, styleProps) : '',CarouselStyle$1 && CarouselStyle$1.getStyleSheet ? CarouselStyle$1.getStyleSheet(undefined, styleProps) : ''
+  BaseStyle$8 && BaseStyle$8.getStyleSheet ? BaseStyle$8.getStyleSheet(undefined, styleProps) : '',ButtonStyle$1 && ButtonStyle$1.getStyleSheet ? ButtonStyle$1.getStyleSheet(undefined, styleProps) : '',CarouselStyle$1 && CarouselStyle$1.getStyleSheet ? CarouselStyle$1.getStyleSheet(undefined, styleProps) : '',InputTextStyle$1 && InputTextStyle$1.getStyleSheet ? InputTextStyle$1.getStyleSheet(undefined, styleProps) : '',InputGroupStyle$1 && InputGroupStyle$1.getStyleSheet ? InputGroupStyle$1.getStyleSheet(undefined, styleProps) : '',TieredMenuStyle$1 && TieredMenuStyle$1.getStyleSheet ? TieredMenuStyle$1.getStyleSheet(undefined, styleProps) : '',MegaMenuStyle$1 && MegaMenuStyle$1.getStyleSheet ? MegaMenuStyle$1.getStyleSheet(undefined, styleProps) : '',BadgeDirectiveStyle$1 && BadgeDirectiveStyle$1.getStyleSheet ? BadgeDirectiveStyle$1.getStyleSheet(undefined, styleProps) : ''
 ].join('');
 
 Theme.setTheme(options?.theme);
 
 const themes = [
-    BaseStyle$3 && BaseStyle$3.getCommonThemeStyleSheet ? BaseStyle$3.getCommonThemeStyleSheet(undefined, styleProps) : '',
-    BaseStyle$3 && BaseStyle$3.getThemeStyleSheet ? BaseStyle$3.getThemeStyleSheet(undefined, styleProps) : '',ButtonStyle$1 && ButtonStyle$1.getThemeStyleSheet ? ButtonStyle$1.getThemeStyleSheet(undefined, styleProps) : '',CarouselStyle$1 && CarouselStyle$1.getThemeStyleSheet ? CarouselStyle$1.getThemeStyleSheet(undefined, styleProps) : ''
+    BaseStyle$8 && BaseStyle$8.getCommonThemeStyleSheet ? BaseStyle$8.getCommonThemeStyleSheet(undefined, styleProps) : '',
+    BaseStyle$8 && BaseStyle$8.getThemeStyleSheet ? BaseStyle$8.getThemeStyleSheet(undefined, styleProps) : '',ButtonStyle$1 && ButtonStyle$1.getThemeStyleSheet ? ButtonStyle$1.getThemeStyleSheet(undefined, styleProps) : '',CarouselStyle$1 && CarouselStyle$1.getThemeStyleSheet ? CarouselStyle$1.getThemeStyleSheet(undefined, styleProps) : '',InputTextStyle$1 && InputTextStyle$1.getThemeStyleSheet ? InputTextStyle$1.getThemeStyleSheet(undefined, styleProps) : '',InputGroupStyle$1 && InputGroupStyle$1.getThemeStyleSheet ? InputGroupStyle$1.getThemeStyleSheet(undefined, styleProps) : '',TieredMenuStyle$1 && TieredMenuStyle$1.getThemeStyleSheet ? TieredMenuStyle$1.getThemeStyleSheet(undefined, styleProps) : '',MegaMenuStyle$1 && MegaMenuStyle$1.getThemeStyleSheet ? MegaMenuStyle$1.getThemeStyleSheet(undefined, styleProps) : '',BadgeDirectiveStyle$1 && BadgeDirectiveStyle$1.getThemeStyleSheet ? BadgeDirectiveStyle$1.getThemeStyleSheet(undefined, styleProps) : ''
 ].join('');
 
 const defineNitroPlugin = (def) => def;
