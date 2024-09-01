34a24bd95322e830c672b5c91560da9c1e84a90b
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-09-01 08:50:23 +0200

    Added logout functionality

--- server/routes/auth/logout.ts
+++ server/routes/auth/logout.ts
@@ -1,9 +1,70 @@
-export default defineEventHandler(async (event)=>{
-    setCookie(event, "user", "");
+import axios from "axios";
+import { SHOP_URL } from "~/services/global.variables";
+import { getCookie, deleteCookie } from "h3";
+let token:any
+let refresh_token:any
+export default defineEventHandler(async (event) => {
+    const response: any = {};
+  
+    try {
+        // Get the cookies (tokens) from the event request
+        token = getCookie(event, "token");
+        refresh_token = getCookie(event, "refresh_token");
+        
+        console.log("Token from cookie:", token);
+        console.log("Refresh token from cookie:", refresh_token);
 
-    setCookie(event, "token", "");
+        // Call the logout function with the tokens
+        await logout();
 
-    return {
-        success: true
+        // Delete all the cookies set during login
+        deleteCookie(event, "token");
+        deleteCookie(event, "refresh_token");
+        deleteCookie(event, "user_id");
+        deleteCookie(event, "username");
+
+        response['success'] = true;
+        response['message'] = "Logged out successfully.";
+    } catch (error: any) {
+        console.error("Logout error:", error); // Log error details
+        response['success'] = false;
+        response['message'] = error.toString();
     }
+
+    return response;
 });
+
+export const logout = async () => {
+    const url = `${SHOP_URL}/api/logout`;
+    const headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
+    
+    const body = {
+        "token": token.replace(/^"|"$/g, ''),
+        "refresh_token": refresh_token.replace(/^"|"$/g, ''),
+    };
+    console.log('my body', body);
+    try {
+        const response = await axios.post(url, body, { headers });
+        console.log("Logout Response Data:", response.data);
+        return response.data;
+    } catch (error: any) {
+        // Handle specific errors orn rethrow for generic handling
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("Server Error:", error.response.data);
+            throw new Error("Server Error. Please try again later.");
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log("Network Error:", error.request);
+            throw new Error("Network Error. Please check your internet connection.");
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Request Error:", error.message);
+            throw new Error("Request Error. Please try again.");
+        }
+    }
+};
