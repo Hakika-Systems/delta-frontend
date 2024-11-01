08b6c636f1774e4d1577240ca15e3568cf6f3a92
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-11-01 12:17:36 +0200

    Login Error Messages solved

--- server/routes/auth/user.ts
+++ server/routes/auth/user.ts
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { SHOP_URL } from "~/services/global.variables";
+
 export default defineEventHandler(async (event) => {
     const { email, password } = await readBody(event);
     const response:any = {};
 
     try {
         const data = await login({ email, password });
-        setCookie(event, "token",JSON.stringify(data?.data?.token));
-        setCookie(event, "refresh_token",JSON.stringify(data?.data?.refresh_token));
-        setCookie(event, "user_id ", JSON.stringify(data?.data?.user.id));
-        setCookie(event, "username ", JSON.stringify(data?.data?.user.name));
+        setCookie(event, "token", JSON.stringify(data?.data?.token));
+        setCookie(event, "refresh_token", JSON.stringify(data?.data?.refresh_token));
+        setCookie(event, "user_id", JSON.stringify(data?.data?.user.id));
+        setCookie(event, "username", JSON.stringify(data?.data?.user.name));
         response['login'] = data;
         response['success'] = true;
     } catch (error:any) {
         response['success'] = false;
-        response['message'] = error.toString();
+        response['message'] = error.response?.data?.message || error.message || "An unexpected error occurred.";
     }
 
     return response;
@@ -35,17 +36,15 @@ export const login = async (data:any) => {
         const response = await axios.post(url, body, { headers });
         return response.data;
     } catch (error:any) {
-        // Handle specific errors or rethrow for generic handling
         if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            throw new Error("Server Error. Please try again later.");
+            // Server responded with a status code out of 2xx range
+            throw new Error(error.response.data?.message || "Server Error. Please try again later.");
         } else if (error.request) {
-            // The request was made but no response was received
+            // Request was made but no response was received
             throw new Error("Network Error. Please check your internet connection.");
         } else {
-            // Something happened in setting up the request that triggered an Error
-            throw new Error("Request Error. Please try again.");
+            // Something else caused the error
+            throw new Error(error.message || "Request Error. Please try again.");
         }
-    };
+    }
 };
