ef180a492b2de011e295ea91723446c67ee77335
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-11-23 09:59:37 +0200

    updated global variable

--- services/global.variables.ts
+++ services/global.variables.ts
@@ -1,2 +1,2 @@
-//export const SHOP_URL =  'https://back-uat.okshopeasy.com' 
-export const SHOP_URL =  'https://api.hakikasystems.co.zw' 
+export const SHOP_URL =  'https://back-uat.okshopeasy.com' 
+//export const SHOP_URL =  'https://api.hakikasystems.co.zw' 
