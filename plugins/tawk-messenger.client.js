c0de51984aea7588c207ee27324412336ed9dcb4
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-12-03 14:00:58 +0200

    tawk

--- plugins/tawk-messenger.client.js
+++ plugins/tawk-messenger.client.js
@@ -5,7 +5,7 @@ export default defineNuxtPlugin(() => {
         var s1 = document.createElement("script");
         var s0 = document.getElementsByTagName("script")[0];
         s1.async = true;
-        s1.src = "https://embed.tawk.to/65d6f4de8d261e1b5f63986c/1ic5vbd7r";
+        s1.src = "https://embed.tawk.to/674d89714304e3196aeb4e69/1ie3e1pjc";
         s1.charset = "UTF-8";
         s1.setAttribute("crossorigin", "*");
         s0.parentNode.insertBefore(s1, s0);
