cdde5af1f7a673d195be91603fa8e375511c1b75
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-12-05 20:29:17 +0200

    added configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
-  runtimeConfig: {
-   
+  site: {
+    url: 'https://okshopeasy.com/',
+    name: 'OK ShopEasy',
+    description: 'Number One Online Store in Zimbabwe',
+    defaultLocale: 'en',
+    redirectToCanonicalSiteUrl: true
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' }
   },
   devtools: { enabled: true },
   compatibilityDate: "2024-07-04",
   css: ["primeflex/primeflex.css"],
-  modules: ["@primevue/nuxt-module",'@pinia/nuxt','nuxt-swiper'],
+  modules: ["@primevue/nuxt-module", '@pinia/nuxt','nuxt-purgecss', 'nuxt-swiper', '@nuxt/image', '@nuxtjs/seo'],
+  purgecss: {
+    safelist: {
+        greedy: [/^nuxt-/, /^vue-/, /^p-/, /^swiper-/, /^swal2-/, /^dt-/, /^dataTables/, /^table/, /^sorting/, /^page-item/, /^page-link/, /^pagination/, /^paginate_button/, /^paging/, /^form-control/, /^form-select/, /^dtfc-fixed-/, /^col-/ ]
+    }
+ },
   plugins: [
     { src: '~/plugins/tawk-messenger.client.js', mode: 'client' }
   ],
