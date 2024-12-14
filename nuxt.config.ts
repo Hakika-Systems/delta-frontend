6fb84c7f55e15e3b6fe7ee130871c73131f0aef2
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-12-14 18:06:34 +0200

    Added security policy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,12 +15,69 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-07-04",
   css: ["primeflex/primeflex.css"],
-  modules: ["@primevue/nuxt-module", '@pinia/nuxt','nuxt-purgecss', 'nuxt-swiper', '@nuxt/image', '@nuxtjs/seo'],
+  modules: ["@primevue/nuxt-module", '@pinia/nuxt','nuxt-purgecss','nuxt-security', 'nuxt-swiper', '@nuxt/image', '@nuxtjs/seo'],
   purgecss: {
     safelist: {
         greedy: [/^nuxt-/, /^vue-/, /^p-/, /^swiper-/, /^swal2-/, /^dt-/, /^dataTables/, /^table/, /^sorting/, /^page-item/, /^page-link/, /^pagination/, /^paginate_button/, /^paging/, /^form-control/, /^form-select/, /^dtfc-fixed-/, /^col-/ ]
     }
  },
+ security: {
+  headers: {
+      contentSecurityPolicy: {
+          'script-src': [
+              "'self'",
+              "'unsafe-inline'",
+              "https://*.google.com",
+              "https://www.gstatic.com",
+              "https://storage.googleapis.com",
+              "https://*.paypal.com",
+              "https://www.googletagmanager.com",
+              "https://www.google-analytics.com",
+              "https://www.pagespeed-mod.com",
+              "https://*.sentry.io"
+          ],
+          'script-src-attr': [
+              "'self'",
+              "'strict-dynamic'"
+          ],
+          'worker-src': [
+              "'self'",
+              "blob:",
+              "https://storage.googleapis.com"
+          ],
+          'img-src': [
+              "'self'",
+              "data:",
+              "https://t.paypal.com",
+              "https://www.paypalobjects.com",
+              "https://googleads.g.doubleclick.net",
+              "https://www.google.com",
+              "https://www.google.co.zw",
+              "https://*.googleapis.com",
+              "https://*.googleusercontent.com",
+              "https://www.facebook.com",
+              "https://*.linkedin.com",
+              "https://api.hakikasystems.co.zw",
+              "https://back-uat.okshopeasy.com"
+          ],
+          'connect-src': [
+              "'self'",
+              "https://api.hakikasystems.co.zw",
+              "https://back-uat.okshopeasy.com",
+              "https://*.google.com",
+              "https://www.googletagmanager.com",
+              "https://www.google-analytics.com",
+              "https://*.paypal.com",
+              "https://*.sentry.io"
+          ]
+      },
+      crossOriginEmbedderPolicy: 'unsafe-none',
+      crossOriginOpenerPolicy: 'same-origin-allow-popups',
+      xFrameOptions: 'SAMEORIGIN'
+  },
+  rateLimiter: false
+},
+
   plugins: [
     { src: '~/plugins/tawk-messenger.client.js', mode: 'client' }
   ],
