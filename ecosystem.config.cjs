534e35ad1ed55ad674466414288c128b5c19d58b
Brian Mhlanga <brianmhlanga9@gmail.com>
2025-02-14 20:52:49 +0200

    added ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'OK Zimbabwe',
-        port: '3000',
-        exec_mode: 'cluster',
-        instances: '1',
-        script: './.output/server/index.mjs' 
+  apps: [
+    {
+      name: 'Delta Beverages',
+      port: '3004',
+      exec_mode: 'cluster',
+      instances: '1',
+      script: './.output/server/index.mjs' ,
+      env: {
+        NODE_ENV: 'production',
+        JWT_SECRET: 'SIL325325zZZxsCdvdhvlV43Sprofml'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        JWT_SECRET: 'SIL325325zZZxsCdvdhvlV43Sprofml'
       }
-    ]
-  }
+    }
+  ]
+}
